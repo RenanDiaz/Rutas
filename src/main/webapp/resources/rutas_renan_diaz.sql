c56a8c2da4e66040a289c2909480e0600e8de4fe
rdiaz <renandiazreyes@gmail.com>
2016-06-28 01:24:06 -0500

    sql

--- src/main/webapp/resources/rutas_renan_diaz.sql
+++ src/main/webapp/resources/rutas_renan_diaz.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.6.24)
 # Database: rutas
-# Generation Time: 2016-06-27 13:59:55 +0000
+# Generation Time: 2016-06-28 06:23:15 +0000
 # ************************************************************
 
 
@@ -36,9 +36,66 @@ LOCK TABLES `marcas` WRITE;
 
 INSERT INTO `marcas` (`id`, `nombre`)
 VALUES
-	(0,'Honda');
+	(1,'Honda'),
+	(2,'Nissan'),
+	(3,'Toyota'),
+	(4,'Hyundai');
 
 /*!40000 ALTER TABLE `marcas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table rutas
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `rutas`;
+
+CREATE TABLE `rutas` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `rutas` WRITE;
+/*!40000 ALTER TABLE `rutas` DISABLE KEYS */;
+
+INSERT INTO `rutas` (`id`, `descripcion`)
+VALUES
+	(1,'San Carlitos - Mall Chiriqui');
+
+/*!40000 ALTER TABLE `rutas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table ubicacion
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `ubicacion`;
+
+CREATE TABLE `ubicacion` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `fechahora` datetime DEFAULT NULL,
+  `ruta` int(11) unsigned DEFAULT NULL,
+  `vehiculo` varchar(6) DEFAULT NULL,
+  `latitud` varchar(20) DEFAULT NULL,
+  `longitud` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_ruta` (`ruta`),
+  KEY `fk_vehiculo` (`vehiculo`),
+  CONSTRAINT `fk_ruta` FOREIGN KEY (`ruta`) REFERENCES `rutas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_vehiculo` FOREIGN KEY (`vehiculo`) REFERENCES `vehiculos` (`placa`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `ubicacion` WRITE;
+/*!40000 ALTER TABLE `ubicacion` DISABLE KEYS */;
+
+INSERT INTO `ubicacion` (`id`, `fechahora`, `ruta`, `vehiculo`, `latitud`, `longitud`)
+VALUES
+	(1,'2016-06-27 22:20:17',1,'123456','8.43206926379766','-82.4187118038335'),
+	(2,'2016-06-27 23:57:04',1,'123456','8.43207640','-82.41858114'),
+	(3,'2016-06-28 01:10:24',1,'837173','0','0');
+
+/*!40000 ALTER TABLE `ubicacion` ENABLE KEYS */;
 UNLOCK TABLES;
 
 
@@ -63,34 +120,14 @@ LOCK TABLES `vehiculos` WRITE;
 
 INSERT INTO `vehiculos` (`placa`, `marca`, `modelo`, `anno`, `tipo`)
 VALUES
-	('123456',0,'City',2016,0);
+	('123456',1,'City',2016,0),
+	('321123',2,'Versa',2014,0),
+	('837173',4,'Elantra',2013,0),
+	('RDR123',1,'Civic',2015,0),
+	('RDR321',3,'Yaris',1999,0);
 
 /*!40000 ALTER TABLE `vehiculos` ENABLE KEYS */;
 UNLOCK TABLES;
-
-
-# Dump of table rutas
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `rutas`;
-
-CREATE TABLE `rutas` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Dump of table ubicacion
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `ubicacion`;
-
-CREATE TABLE `ubicacion` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 
 
